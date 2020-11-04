276b29aa6b1afb30a50e111d76fa1c43a6d245b0
christopher ingram <cingram82@gmail.com>
2020-11-04 16:00:30 -0500

    Enemy movement

---
+++ Assets/Scripts/Enemies/EnemyGroundMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Used for all ground based enemy movement.
+/// Will walk till it hits a wall or platform edge and flips direction
+/// Used the OnExitTrigger to know when exiting the groud collider to flip
+/// 
+/// NOTE: Set up to work with sprites that face right by default
+/// </summary>
+public class EnemyGroundMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    private Rigidbody2D rb2D;
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb2D.velocity = new Vector2((transform.localScale.x > 0) ? moveSpeed : -moveSpeed, 0f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-Mathf.Sign(rb2D.velocity.x), 1f);
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] int health = 1;
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health = Mathf.Max(0,health -damage);
+        anim.SetTrigger("isHit");
+        anim.SetInteger("Hp", health);
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
