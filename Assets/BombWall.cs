0985fe62470042c34c385306067fa9cc40374432
christopher ingram <cingram82@gmail.com>
2020-11-12 09:21:41 -0500

    Training level set up

---
+++ Assets/BombWall.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombWall : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Explosion"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
