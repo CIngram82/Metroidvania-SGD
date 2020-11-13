825eaa04e53ef5ff89f2ceb5ce5a4002c30659fc
christopher ingram <cingram82@gmail.com>
2020-11-13 15:36:02 -0500

    Testing Door based transitions

---
+++ Assets/OpenDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<Animator>().SetTrigger("Open");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            GetComponent<Animator>().SetTrigger("Close");
+        }
+    }
+
+
+}
---
+++ Assets/RoomTransition.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTransition : MonoBehaviour
+{
+    [SerializeField] GameObject doorRight;
+    [SerializeField] GameObject doorLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if(other.transform.position.x > transform.position.x)
+            {
+                other.transform.Translate(Vector3.left * 3);
+            }
+            else
+            {
+                other.transform.Translate(Vector3.right * 3);
+            }
+            
+        }
+    }
+ 
+
+}
