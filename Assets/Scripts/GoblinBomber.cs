f70a4498d2fb0c7819ea6b4ce0aab408a0165f8c
Destiney <dsprice1@my.waketech.edu>
2020-11-19 21:55:21 -0500

    Update GoblinBomber.cs
    Just added the ability for the goblin bomber to check how close the player is.

--- Assets/Scripts/GoblinBomber.cs
+++ Assets/Scripts/GoblinBomber.cs
@@ -8,13 +8,21 @@ public class GoblinBomber : MonoBehaviour
     public static GoblinBomber goblinBomber;
     private int health;
     [SerializeField] private Animator anim;
+    private GameObject playerobj;
+    private GameObject thisObject;
+    private float distance;
+    private bool isFinding;
 
-    
+
+
     void Start()
     {
         goblinBomber = this;
         health = 5;
         anim = gameObject.GetComponent<Animator>();
+        playerobj = GameObject.Find("Player");  
+        isFinding = true;
+        StartCoroutine(FindplayerLocation());
     }
 
    public void TakeGoblinDamage()
@@ -25,11 +33,34 @@ public class GoblinBomber : MonoBehaviour
         
         if(health == 0)
         {
+            isFinding = false; 
             anim.SetBool("IsDead", false);
             gameObject.SetActive(false);
         }
    }
-  
+
+
+    IEnumerator FindplayerLocation()
+    {
+
+        while (isFinding) 
+        {
+
+            distance = Vector3.Distance(gameObject.transform.position, playerobj.transform.position);  
+            
+            if (distance < 2)
+            {
+                Debug.Log("In Range to close attack");
+            }
+            else if(distance < 5)
+            {
+                Debug.Log("In Range To Bomb");
+            }
+            yield return null;
+
+        }
+        yield return new WaitForSeconds(1);
+    }
 
 
 }
