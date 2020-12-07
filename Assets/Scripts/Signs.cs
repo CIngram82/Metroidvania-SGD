4f511ce6e8e563118478b7be7151455ce5c6d127
Le0nardLChurch <59894914+Le0nardLChurch@users.noreply.github.com>
2020-12-07 16:27:48 -0500

    Signs prefab added.

---
+++ Assets/Scripts/Signs.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Signs : MonoBehaviour
+{
+    enum SignType { Image, Text }
+    [Header("Sign Display Type")]
+    [SerializeField] SignType signType;
+    [Header("Image")]
+    [SerializeField] int spriteIndex;
+    [SerializeField] SpriteRenderer signImage;
+    [SerializeField] Sprite[] itemSprites;
+    [Header("Text")]
+    [SerializeField] string text;
+    [SerializeField] TextMeshPro signText;
+
+
+    void Start()
+    {
+        if (signType == SignType.Image)
+        {
+            signText.gameObject.SetActive(false);
+            signImage.gameObject.SetActive(true);
+            signImage.sprite = itemSprites[spriteIndex];
+        }
+        else
+        {
+            signImage.gameObject.SetActive(false);
+            signText.gameObject.SetActive(true);
+            signText.text = text;
+        }
+    }
+}
+
+
+
+
+
