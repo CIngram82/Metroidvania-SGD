e26ce2f5ea0bdd538957fef6b710aa2d83d01719
christopher ingram <cingram82@gmail.com>
2020-12-09 15:03:33 -0500

    Button added

---
+++ Assets/Scripts/ButtonJoke.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonJoke : MonoBehaviour
+{
+
+    public void OpenLink(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
